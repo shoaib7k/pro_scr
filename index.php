99fbaf32484f3f6b60dd8df86f6101722ca52f2c
Shoaib Bin Anwar <shoaib7k@live.com>
2020-11-11 02:22:47 +0100

    layout edit header done

--- index.php
+++ index.php
@@ -8,30 +8,7 @@
         <script type="text/javascript" src="./js/bootstrap.min.js"></script>
     </head>
 <body>
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-        <!-- <h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5> -->
-        <a href="" class="my-2 my-md-0 mr-md-auto"><img src="./images/rf-logo-intranet.svg"  alt="Responsive image"></a>
-        <nav class="my-2 my-md-0 mr-md-3">
-          <a class="p-2 text-dark" href="#">Features</a>
-          <a class="p-2 text-dark" href="#">Enterprise</a>
-          <a class="p-2 text-dark" href="#">Support</a>
-          <a class="p-2 text-dark" href="#">Pricing</a>
-         
-        <a class="btn btn-outline-primary" href="#">Sign up</a>
-                            <img src='images/button-login.svg' name='button-login' onmouseover="this.src='images/button-login-red.svg'" onmouseout="this.src='images/button-login.svg'" onclick='' class="img_button_40"/>
-                        
-          <a href="settings.php" class="btn btn-outline-primary"><img src="images/button-gear.svg" alt="button config" onmouseover="javascript:this.src='images/button-gear-red.svg'" onmouseout="javascript:this.src='images/button-gear.svg'" class="svg_image_button_2"></a>
-          
-        <select class="selectpicker" data-width="fit">
-            <option data-content='<span class="flag-icon flag-icon-us"></span> English'>English</option>
-          <option  data-content='<span class="flag-icon flag-icon-mx"></span> Español'>Español</option>
-        </select>
-        </nav>
-  
-        
-            
-          
-      </div>
+    <?php include "header.php" ?>
   
      
   
